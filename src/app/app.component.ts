a7959cec431e8077e53bb0f6f64e3a3df3161204
David Allen <davetvmusic@hotmail.com>
2020-07-08 18:02:48 +1000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { CanComponentLeave } from './unsaved-changes.guard';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'route-guard';
+export class AppComponent implements CanComponentLeave {
+  
+  public canLeave(): boolean {
+    if ('isInEdit') {
+      // show dialog and get value (true leave, false stay);
+    }
+    return true;
+  }
 }
---
+++ src/app/unsaved-changes.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanDeactivate } from '@angular/router';
+
+export interface CanComponentLeave {
+  canLeave: () => boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UnsavedChangesGuard implements CanDeactivate<CanComponentLeave> {
+
+  canDeactivate(component: CanComponentLeave) {
+    if (component.canLeave) {
+      return component.canLeave();
+    }
+    return true;
+  }
+}
